eb76bae63d9706f27f0ab635c945ed7a67c66411
jorgensandsabo <48399478+jorgensandsabo@users.noreply.github.com>
2020-04-21 23:15:34 +0200

    Functionality to download only DEM or RGB rasters

--- get_gee.R
+++ get_gee.R
@@ -12,7 +12,12 @@ get_gee <- function(region, shape = "square", buffer.width = 1000, max.error = 1
                     dem = "JAXA/ALOS/AW3D30/V2_2", dem.band = "AVE_DSM", demfunc = NULL, demtype = "image",
                     rgb = "LANDSAT/LC08/C01/T1", red.band = "B4", green.band = "B3", blue.band = "B2", rgbfunc = landsatCF, rgbtype = "imagecollection",
                     ...){
-
+  
+  # Checks
+  if(is.null(dem) & is.null(rgb)){
+    stop("Need to provide Earth Engine Snippet to an image or image collection representing elevation and/or an image or image collection representing RGB values")  
+  }
+  
   # Get geometry of region of interest
   if(is.vector(region) & length(region) == 2){
     coords <- list(cbind(region[1],region[2])[1,])
@@ -47,51 +52,70 @@ get_gee <- function(region, shape = "square", buffer.width = 1000, max.error = 1
   }
   
   # Get elevation raster from Earth Engine
-  if(demtype == "imagecollection"){
-    DEM_full <- ee$ImageCollection(dem)
-    if(!is.null(demfunc)){
-      DEM_full <- demfunc(DEM_full)
+  if(!is.null(dem)){
+    if(demtype == "imagecollection"){
+      DEM_full <- ee$ImageCollection(dem)
+      if(!is.null(demfunc)){
+        DEM_full <- demfunc(DEM_full)
+      }
+    }else if(demtype == "image"){
+      DEM_full <- ee$Image(dem)
     }
-  }else if(demtype == "image"){
-    DEM_full <- ee$Image(dem)
+    DEM <- DEM_full$select(list(dem.band),list("elevation"))
+  }else{
+    rasterElev <- NULL
   }
-  DEM <- DEM_full$select(list(dem.band),list("elevation"))
-  
+
   # Get cloud-free landsat raster from Earth Engine
-  if(rgbtype == "imagecollection"){
-    RGB_full <- ee$ImageCollection(rgb)
-    if(!is.null(rgbfunc)){
-      RGB_full <- rgbfunc(RGB_full, ...)
+  if(!is.null(rgb)){
+    if(rgbtype == "imagecollection"){
+      RGB_full <- ee$ImageCollection(rgb)
+      if(!is.null(rgbfunc)){
+        RGB_full <- rgbfunc(RGB_full, ...)
+      }
+    }else if(rgbtype == "image"){
+      RGB_full <- ee$Image(rgb)
     }
-  }else if(rgbtype == "image"){
-    RGB_full <- ee$Image(rgb)
+    RGB <- RGB_full$select(list(red.band,green.band,blue.band),list("red","green","blue"))
+  }else{
+    rasterRGB <- NULL
   }
-  RGB <- RGB_full$select(list(red.band,green.band,blue.band),list("red","green","blue"))
   
   # Reduce rasters to roi
-  latlng <- ee$Image$pixelLonLat()$addBands(DEM)$addBands(RGB)
+  if(!is.null(dem) & is.null(rgb)){
+    latlng <- ee$Image$pixelLonLat()$addBands(DEM)
+  }else if(!is.null(rgb) & is.null(dem)){
+    latlng <- ee$Image$pixelLonLat()$addBands(RGB)
+  }else if(!is.null(dem) & !is.null(rgb)){
+    latlng <- ee$Image$pixelLonLat()$addBands(DEM)$addBands(RGB)
+  }
+  
   latlng <- latlng$reduceRegion(reducer = ee$Reducer$toList(),
                                 geometry = roi,
                                 maxPixels = max.pixels,
                                 scale=scale)
   
-  # Convert to arrays
+  # Convert to arrays and raster/rasterBrick
   lats <- np$array((ee$Array(latlng$get("latitude"))$getInfo()))
   lngs <- np$array((ee$Array(latlng$get("longitude"))$getInfo()))
-  elevs <- np$array((ee$Array(latlng$get("elevation"))$getInfo()))
   
-  red <- np$array((ee$Array(latlng$get("red"))$getInfo()))
-  green <- np$array((ee$Array(latlng$get("green"))$getInfo()))
-  blue <- np$array((ee$Array(latlng$get("blue"))$getInfo()))
-  
-  # Convert to elevation raster
-  elevation <- data.frame(x = lngs, y = lats, elevation = elevs)
-  rasterElev <- raster::rasterFromXYZ(elevation,crs="+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
-  
-  # Convert landsat to rasterBrick
-  rasterRGB <- raster::brick(list(raster::rasterFromXYZ(data.frame(x = lngs, y = lats, red = red),crs="+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"),
-                                  raster::rasterFromXYZ(data.frame(x = lngs, y = lats, green = green),crs="+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"),
-                                  raster::rasterFromXYZ(data.frame(x = lngs, y = lats, blue = blue),crs="+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")))
+  if(!is.null(dem)){
+    elevs <- np$array((ee$Array(latlng$get("elevation"))$getInfo()))
+    
+    # Convert to elevation raster
+    elevation <- data.frame(x = lngs, y = lats, elevation = elevs)
+    rasterElev <- raster::rasterFromXYZ(elevation,crs="+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
+  }
+  if(!is.null(rgb)){
+    red <- np$array((ee$Array(latlng$get("red"))$getInfo()))
+    green <- np$array((ee$Array(latlng$get("green"))$getInfo()))
+    blue <- np$array((ee$Array(latlng$get("blue"))$getInfo()))
+    
+    # Convert landsat to rasterBrick
+    rasterRGB <- raster::brick(list(raster::rasterFromXYZ(data.frame(x = lngs, y = lats, red = red),crs="+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"),
+                                    raster::rasterFromXYZ(data.frame(x = lngs, y = lats, green = green),crs="+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"),
+                                    raster::rasterFromXYZ(data.frame(x = lngs, y = lats, blue = blue),crs="+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")))
+  }
   
   # Return elevation and landsat rasters
   return(list(rasterElev = rasterElev, rasterRGB = rasterRGB))
